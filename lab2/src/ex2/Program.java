438e584f5273b32c82b825033d6062035fdeeec6
ana <anaionita.fly@gmail.com>
2025-03-08 10:47:57 +0200

    Update pb 2/ lab2

--- lab2/src/ex2/Program.java
+++ lab2/src/ex2/Program.java
@@ -6,21 +6,17 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Program {
+
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new File("src/ex2/cantec_in.txt"));
         PrintStream flux_out = new PrintStream("src/ex2/cantec_out.txt");
 
-        String vers;
-        Vers versulet = new Vers();
         Vers[] vectorVersuri = new Vers[30];
         int contor=0;
         while (scan.hasNextLine()) {
-            vers = scan.nextLine();
-            int nrCuv = versulet.NrCUv(vers);
-            int nrVocale= versulet.NrVocale(vers);
-            String s=vers +" "+ String.valueOf(nrCuv)+" cuvinte si "+String.valueOf(nrVocale)+" vocale";
-            vectorVersuri[contor]=new Vers(nrCuv, nrVocale, s);
-            flux_out.println(vectorVersuri[contor].getVers());
+            String vers = scan.nextLine();
+            vectorVersuri[contor]=new Vers(vers);
+            flux_out.println(vectorVersuri[contor].toString());
             contor++;
         }
     }
--- lab2/src/ex2/Vers.java
+++ lab2/src/ex2/Vers.java
@@ -1,51 +1,61 @@
 package ex2;
 
 public class Vers {
-    public int nrCuv;
-    public int nrVocale;
+    private int nrCuv;
+    private int nrVocale;
     private String vers;
     //*
     //nr aleator<0.1 =>majuscule
 
 
+    public Vers(int nrCuv, int nrVocale, String vers) {
+        this.nrVocale = nrVocale;
+        this.nrCuv = nrCuv;
+        this.vers = vers;
+    }
+
+    public Vers(String vers) {
+        this.vers = vers;
+        this.nrCuv = initCuv();
+        this.nrVocale = initNrVocale();
+    }
+
+    public Vers() {
+
+    }
+
     public String getVers() {
         return vers;
     }
 
-    public int NrCUv(String vers) {
+    public void setVers(String vers) {
+        this.vers = vers;
+    }
+
+    public int initCuv() {
         String[] words = vers.split("\\s+");
         nrCuv = words.length;
         return nrCuv;
     }
 
-    public Vers(int nrCuv, int nrVocale, String vers)
-{
-    this.nrVocale= nrVocale;
-    this.nrCuv= nrCuv;
-    this.vers=vers;
-}
-
-    public Vers()
-    {
-
-    }
-    public int NrVocale(String vers)
-    {
+    public int initNrVocale() {
         int n = vers.length();
-        nrVocale=0;
-        for (int i = 0; i < n; i++)
-        {
+        nrVocale = 0;
+        for (int i = 0; i < n; i++) {
             if (vers.toUpperCase().charAt(i) == 'A' ||
                     vers.toUpperCase().charAt(i) == 'E' ||
                     vers.toUpperCase().charAt(i) == 'I' ||
                     vers.toUpperCase().charAt(i) == 'O' ||
-                    vers.toUpperCase().charAt(i) == 'U')
-            {
+                    vers.toUpperCase().charAt(i) == 'U') {
                 nrVocale++;
             }
 
         }
         return nrVocale;
+    }
 
+    @Override
+    public String toString() {
+        return String.format("%s are %s cuvinte si %s vocale", vers, nrCuv, nrVocale);
     }
 }
